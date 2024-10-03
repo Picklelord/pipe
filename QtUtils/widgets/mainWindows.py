b4a04d7a1f7efbc480208ca142db46bc1bce7cb4
Daniel <d.garnier.live@gmail.com>
2024-10-03 12:41:42 +0930

    Update mainWindows.py Add black formating and adjust base variables

--- QtUtils/widgets/mainWindows.py
+++ QtUtils/widgets/mainWindows.py
@@ -6,9 +6,9 @@ class Window(QtWid.QMainWindow):
         super(Window, sf).__init__()
 
     def initUI(sf, **kwargs):
-        """ 
+        """
         initialises the UI and sets up base UI elements for a Window with a tab Widget
-            
+
         kwargs: title,            (str),      title text for the UI
         kwargs: left,             (int),      UI default position left point
         kwargs: top,              (int),      UI default position top point
@@ -16,16 +16,16 @@ class Window(QtWid.QMainWindow):
         kwargs: height,           (int),      UI default height
         kwargs: setCentralWidget, (bool),     if true will set the main widget as the central widget
         kwargs: setMainWidget,    (QWidget),  pass your QWidget to be used as the main widget
-        kwargs: layoutType,       (str enum), sets layout type: vertical or horizontal 
+        kwargs: layoutType,       (str enum), sets layout type: vertical or horizontal
         kwargs: menu,             (bool),     if true will add a menuBar to the UI
         kwargs: statusBar,        (bool),     if true will add a statusBar to the UI
-            
+
         """
         title = kwargs.pop("title", "TITLE MISSING!")
         left = kwargs.pop("left", 0)
         top = kwargs.pop("top", 0)
-        width = kwargs.pop("width", 290)
-        height = kwargs.pop("height", 80)
+        initwidth = kwargs.pop("width", 290)
+        initheight = kwargs.pop("height", 80)
         setCentralWidget = kwargs.pop("setCentralWidget", True)
         setMainWidget = kwargs.pop("setMainWidget", QtWid.QWidget(sf))
         layoutType = kwargs.pop("layoutType", "vertical")
@@ -34,7 +34,7 @@ class Window(QtWid.QMainWindow):
 
         sf.setWindowTitle(title)
         sf.setObjectName(title.replace(" ", "_"))
-        sf.setMinimumSize(width, height)
+        sf.setMinimumSize(initwidth, initheight)
         if menuBool:
             sf.menubar = sf.menuBar()
 
@@ -54,7 +54,7 @@ class Window(QtWid.QMainWindow):
             sf.main.setLayout(sf.mainLayout)
 
     def run(sf):
-        """ runs/shows the UI """
+        """runs/shows the UI"""
         sf.show()
 
 
@@ -63,9 +63,9 @@ class TabWindow(QtWid.QMainWindow):
         super(TabWindow, sf).__init__()
 
     def initUI(sf, **kwargs):
-        """ 
+        """
         initialises the UI and sets up base UI elements for a Window with a tab Widget
-            
+
         kwargs: title,            (str),      title text for the UI
         kwargs: left,             (int),      UI default position left point
         kwargs: top,              (int),      UI default position top point
@@ -73,27 +73,30 @@ class TabWindow(QtWid.QMainWindow):
         kwargs: height,           (int),      UI default height
         kwargs: setCentralWidget, (bool),     if true will set the main widget as the central widget
         kwargs: setMainWidget,    (QWidget),  pass your QWidget to be used as the main widget
-        kwargs: layoutType,       (str enum), sets layout type: vertical or horizontal 
-        kwargs: tabLayoutType,    (str enum), sets tab page layout type: vertical or horizontal 
+        kwargs: layoutType,       (str enum), sets layout type: vertical or horizontal
+        kwargs: tabLayoutType,    (str enum), sets tab page layout type: vertical or horizontal
         kwargs: menu,             (bool),     if true will add a menuBar to the UI
         kwargs: statusBar,        (bool),     if true will add a statusBar to the UI
-            
+
         """
         title = kwargs.pop("title", "TITLE MISSING!")
-        left = kwargs.pop("left", 0)
-        top = kwargs.pop("top", 0)
-        width = kwargs.pop("width", 290)
-        height = kwargs.pop("height", 80)
+        left = kwargs.pop("left", 200)
+        top = kwargs.pop("top", 200)
+        initwidth = kwargs.pop("width", 290)
+        initheight = kwargs.pop("height", 80)
         setCentralWidget = kwargs.pop("setCentralWidget", True)
         setMainWidget = kwargs.pop("setMainWidget", QtWid.QWidget(sf))
         layoutType = kwargs.pop("layoutType", "vertical")
         sf.tabLayoutType = kwargs.pop("tabLayoutType", "vertical")
         menuBool = kwargs.pop("menu", False)
         statusBarBool = kwargs.pop("statusBar", False)
+        setMinimumSizeBool = kwargs.pop("setMinimumSize", True)
 
         sf.setWindowTitle(title)
         sf.setObjectName(title.replace(" ", "_"))
-        sf.setMinimumSize(width, height)
+        if setMinimumSizeBool:
+            sf.setMinimumSize(initwidth, initheight)
+        sf.setGeometry(left, top, initwidth, initheight)
         if menuBool:
             sf.menubar = sf.menuBar()
 
@@ -114,9 +117,10 @@ class TabWindow(QtWid.QMainWindow):
         sf.tabWidget = QtWid.QTabWidget()
         sf.mainLayout.addWidget(sf.tabWidget)
         sf.tabs = {}
+        sf.tabIdToKey = {}
 
     def run(sf):
-        """ runs/shows the UI """
+        """runs/shows the UI"""
         sf.show()
 
     def addTab(sf, key="dft", name="default", widget=QtWid.QWidget):
@@ -129,13 +133,14 @@ class TabWindow(QtWid.QMainWindow):
             widget, (QWidget), optional, this is the main widget that is used in the Tab Page
 
         returns: sf.tabs[key], the tabs UI elements dict
-        
+
         """
         if key not in sf.tabs:
             sf.tabs[key] = {}
         sf.tabs[key]["widget"] = widget()
         sf.tabs[key]["widget"].setObjectName("tabBG")
         sf.tabs[key]["tabId"] = sf.tabWidget.addTab(sf.tabs[key]["widget"], name)
+        sf.tabIdToKey[sf.tabs[key]["tabId"]] = key
         if sf.tabLayoutType == "vertical":
             sf.tabs[key]["layout"] = QtWid.QVBoxLayout()
         else:
@@ -153,3 +158,4 @@ class TabWindow(QtWid.QMainWindow):
         del sf.tabs[key]["layout"]
         del sf.tabs[key]["widget"]
         sf.tabs[key] = {}
+        sf.tabIdToKey[sf.tabs[key]["tabId"]] = None
